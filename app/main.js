2b5d482c3b2568c93fab8ad89f92de5a457884bf
Kurtis Houser <houserkurtis@gmail.com>
2017-11-07 10:18:17 -0800

    Add basic app framework

---
+++ app/main.js
@@ -0,0 +1,9 @@
+/** Main UI Controller Class */
+class ViewController {
+  /** Initialize Application */
+  constructor () {
+    console.log('App loaded');
+  }
+}
+
+window.ctrl = new ViewController();
