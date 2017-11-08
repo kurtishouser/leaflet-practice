978fbfe269af06c23771f6f14004b9c2614a0ae2
Kurtis Houser <houserkurtis@gmail.com>
2017-11-08 14:33:54 -0800

    Add more content and Update/Delete buttons to popup

--- app/components/map/map.js
+++ app/components/map/map.js
@@ -76,7 +76,26 @@ export class Map extends Component {
 
   addMarkers (markers) {
     markers.forEach(m => {
-      L.marker([m.coordinates[1], m.coordinates[0]]).addTo(this.map).bindPopup(m.title);
+      const layer = L.marker([m.coordinates[1], m.coordinates[0]]).addTo(this.map);
+      layer._id = m._id;
+      layer.title = m.title;
+      layer.description = m.description
+      layer.author = m.author;
+      layer.asset = m.asset;
+      this.onEachLocation(layer);
     });
+  }
+
+  onEachLocation (layer) {
+    console.log('layer', layer);
+    var update_btn = `<button class ='update btn'>Update</button>`
+    var delete_btn = `<button class ='delete btn'>Delete</button>`
+    var saveUpdates_btn = `<button class ='save_updates btn' style='display: none'>Save Changes</button>`
+    var popupContent = `<div style="text-align: left;"><div>Title: ${layer.title}</div><div>Description: ${layer.description}</div><div>Author: ${layer.author}</div><div>Asset: ${layer.asset}</div></div>${update_btn} ${delete_btn} ${saveUpdates_btn} `
+    layer.bindPopup(popupContent);
+    layer.on({ click: (e) => {
+      const { _id, title, description, author, asset } = layer;
+      this.triggerEvent('locationSelected', { _id, title, description, author, asset });
+    }})
   }
 }
